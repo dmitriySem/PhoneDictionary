41ca673605b55b8926e642fdb0bd868b77ae1b29
Dmitriy Semenov <dsemenov@saec.ru>
2024-02-18 00:07:13 +0500

    Добавил копирование контакта

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def input_patronymic():
     return input('Введите отчество контакта: ').title()
 
 def input_phone():
-    return input('Введите телефон контакта: ').title()
+    return input('Введите телефон контакта в формате +7###-###-##-##: ')
 
 def input_adress():
     return input('Введите адрес(город) контакта: ').title()
@@ -24,14 +24,15 @@ def create_contact():
 def add_contact():
     with open('phonebook.txt', 'a', encoding='utf-8') as file:
         file.write(create_contact())
+    print('Контакт добавлен в телефонный правочник')
+    print()
 
 
 
 def print_contact():
-    with open('phonebook.txt', 'r', encoding='utf-8') as file:
-        
+    with open('phonebook.txt', 'r', encoding='utf-8') as file:        
         for number, letter in enumerate(file.read().rstrip().replace(':', '').split('\n\n'), 1):
-            print(''.join('{} {}').format(number, letter))
+            print(number, letter)
         # print(file.read())
    
         
@@ -61,7 +62,26 @@ def search_contact():
             if search in list_contact[i_var]:
                 print(str_contact)
 
-
+def copy_contact():
+    print_contact()
+    flag = True
+    while flag:
+        num_str = input('Выберите какой контакт скопировать в другой файл:')
+        if num_str.isdigit():
+            with open('phonebook.txt', 'r', encoding='utf-8') as file:
+                contacts_list = file.read().rstrip().replace(':', '').split('\n\n')
+                with open('new_pthonebook.txt', 'a', encoding='utf-8') as new_file:
+                    new_file.write(contacts_list[int(num_str)-1]+'\n')
+                    print('Контакт скопирован в другой телефонный справочник!')          
+        else:
+            print('Не коректный ввод!')
+        # Можно дабавить проверку на корректность ввода
+        flag_str = input('Скопировать еще контакт в другой файл? (Да/Нет или 1/0): ')
+        if(flag_str == 'Нет' or flag_str == '0'):
+            flag = False
+        # print(flag)
+          
+        
 
 def interface():
     with open('phonebook.txt', 'a', encoding='utf-8'):
@@ -74,25 +94,33 @@ def interface():
             '1. Добавить контакт \n'
             '2. Вывести на экран \n'
             '3. Поиск контакта \n'
-            '4. Выход '
+            '4. Скопировать контакт \n'
+            '5. Выход '
             )
         print()
         
         var = input('Выберите вариант действий: ')
-        while var not in ('1', '2', '3', '4'):
+        while var not in ('1', '2', '3', '4', '5'):
             print('Некоректный ввод!')
             var = input('Выберите вариант действий: ')
 
         match var:
             case '1':
                 add_contact()
+                return True
             case '2':
                 print_contact()
+                return True
             case '3':
                 search_contact()
+                return True
             case '4':
+                copy_contact()
+                return True
+            case '5':
                 print('До cвидания')
-                exit
+                return False
+                
         print()
 
 
@@ -100,4 +128,6 @@ def interface():
 
     
 if __name__ == '__main__':
-    interface()
+    exit = True
+    while exit:
+        exit = interface()
